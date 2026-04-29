d3679e90668c1881b6e4eb459a902deac2dd5c53
reyhanehsarkhani <reyhanesarkhani@gmail.com>
2026-04-29 17:13:16 +0200

    loading and optimizing font

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,13 @@ import Logo from "./_components/Logo";
 import Navigation from "./_components/Navigation";
 import "@/app/_styles/globals.css";
 
+import { Josefin_Sans } from "next/font/google";
+
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
+console.log(josefin);
 export const metadata = {
   title: {
     template: "%s / The Wild Oasis",
@@ -14,7 +21,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-primary-900 text-primary-50 min-h-screen">
+      <body
+        className={`${josefin.className} bg-primary-900 text-primary-50 min-h-screen`}
+      >
         <header>
           <Logo />
           <Navigation />
