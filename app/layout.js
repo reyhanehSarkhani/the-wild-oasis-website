e5d8338b6a07f7cb4cdbb7882f7efaaa4b976809
reyhanehsarkhani <reyhanesarkhani@gmail.com>
2026-04-29 18:01:45 +0200

    build header and navigation

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import Header from "./_components/Header";
 import Logo from "./_components/Logo";
 import Navigation from "./_components/Navigation";
 import "@/app/_styles/globals.css";
@@ -22,14 +23,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${josefin.className} bg-primary-900 text-primary-50 min-h-screen`}
+        className={`${josefin.className} bg-primary-900 text-primary-50 min-h-screen flex flex-col`}
       >
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
-        <main>{children}</main>
-        <footer>Copyright by The Wild Oasis</footer>
+        <Header />
+        <div className="flex-1 px-8 py-12">
+          <main className="mx-auto max-w-7xl ">{children}</main>
+        </div>
       </body>
     </html>
   );
