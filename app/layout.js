5f3a5c2aea135651e742dfba7b5af371847f099f
reyhanehsarkhani <reyhanesarkhani@gmail.com>
2026-04-29 16:36:08 +0200

    customize tailwind colors and install hero icons

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import Logo from "./_components/Logo";
 import Navigation from "./_components/Navigation";
+import "@/app/_styles/globals.css";
 
 export const metadata = {
   title: "The Wild Oasis",
@@ -8,7 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className="bg-primary-900 text-primary-50 min-h-screen">
         <header>
           <Logo />
           <Navigation />
