bd87c479c6eb696b1092b815b5496a2e1b6c6c59
reyhanehsarkhani <reyhanesarkhani@gmail.com>
2026-05-14 12:41:42 +0200

    install next-auth

---
+++ app/_lib/auth.js
@@ -0,0 +1,22 @@
+import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
+
+const authConfig = {
+  providers: [
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
+      //   authorization: {
+      //     params: {
+      //       prompt: "consent",
+      //       access_type: "offline",
+      //       response_type: "code",
+      //     },
+      //   },
+    }),
+  ],
+};
+export const {
+  auth,
+  handlers: { GET, POST },
+} = NextAuth(authConfig);
